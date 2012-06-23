13311c66e3900b565b332ffb8aa9979d3cfa462d
Micah Carrick <micah@quixotix.com>
2012-06-22 21:56:41 -0700

    Finished window widgets and justification actions.

--- bloatpad.py
+++ bloatpad.py
@@ -1,6 +1,6 @@
 # coding=utf8
 import sys
-from gi.repository import Gtk, Gio, GLib
+from gi.repository import Gtk, Gio, GLib, Gdk
 
 MENU_XML = """
 <interface>
@@ -63,15 +63,109 @@ class Window(Gtk.ApplicationWindow):
                                        default_width=640,
                                        default_height=480,
                                        title="Python Bloatpad")
-                                       
+        # create window ui
+        
+        toolbar = Gtk.Toolbar()
+        
+        button = Gtk.ToggleToolButton.new_from_stock(Gtk.STOCK_JUSTIFY_LEFT)
+        button.set_detailed_action_name("win.justify::left")
+        toolbar.add(button)
+        
+        button = Gtk.ToggleToolButton.new_from_stock(Gtk.STOCK_JUSTIFY_CENTER)
+        button.set_detailed_action_name("win.justify::center")
+        toolbar.add(button)
+        
+        button = Gtk.ToggleToolButton.new_from_stock(Gtk.STOCK_JUSTIFY_RIGHT)
+        button.set_detailed_action_name("win.justify::right")
+        toolbar.add(button)
+        
+        button = Gtk.SeparatorToolItem(draw=False, hexpand=True)
+        button.set_expand(True)
+        toolbar.add(button)
+        
+        switch = Gtk.Switch()
+        switch.set_action_name("win.fullscreen")
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        box.add(Gtk.Label("Fullscreen:"))
+        box.add(switch)
+        button = Gtk.ToolItem()
+        button.add(box)
+        toolbar.add(button)
+
+        scrolled = Gtk.ScrolledWindow(hexpand=True, vexpand=True)
+        textview = Gtk.TextView()
+        scrolled.add(textview)
+        
+        grid = Gtk.Grid()
+        grid.attach(toolbar, 0, 0, 1, 1)
+        grid.attach(scrolled, 0, 1, 1, 1) 
+        
+        self.add(grid)
+        self.show_all()
+        
         # Gtk.ActionMap.add_action_entries() sure would be nice....
         # do we really want to be playing with GVariants in python?
+        
+        """
+        static GActionEntry win_entries[] = {
+          { "copy", window_copy, NULL, NULL, NULL },
+          { "paste", window_paste, NULL, NULL, NULL },
+          { "fullscreen", activate_toggle, NULL, "false", change_fullscreen_state },
+          { "justify", activate_radio, "s", "'left'", change_justify_state }
+        };
+        """
+        # setup actions
+        action = Gio.SimpleAction(name="copy")
+        action.connect("activate", self.copy, textview)        
+        self.add_action(action)
+        
+        # is the UI XML "parse" intentionally?
+        action = Gio.SimpleAction(name="paste")
+        action.connect("activate", self.paste, textview)        
+        self.add_action(action)
+        
         action = Gio.SimpleAction.new_stateful("fullscreen", None, 
-                                               GLib.Variant("b", False))
+                                               GLib.Variant('b', False))
         action.connect("activate", self.activate_toggle)
         action.connect("change-state", self.change_fullscreen_state)
         self.add_action(action)
+        
+        variant = GLib.Variant('s', "left")
+        action = Gio.SimpleAction.new_stateful("justify", variant.get_type(), 
+                                               variant)
+        action.connect("activate", self.activate_radio)
+        action.connect("change-state", self.change_justify_state, textview)
+        self.add_action(action)
+
+    def activate_radio(window, action, parameter, data=None):
+        action.change_state(parameter)
     
+    def change_justify_state(self, action, state, textview):
+        if state.get_string() == "left":
+            textview.set_justification(Gtk.Justification.LEFT)
+        elif state.get_string() == "center":
+            textview.set_justification(Gtk.Justification.CENTER)
+        elif state.get_string() == "right":
+            textview.set_justification(Gtk.Justification.RIGHT)
+        else:
+            return
+        
+        action.set_state(state);
+        """
+        str = g_variant_get_string (state, NULL);
+          if (g_str_equal (str, "left"))
+            gtk_text_view_set_justification (text, GTK_JUSTIFY_LEFT);
+          else if (g_str_equal (str, "center"))
+            gtk_text_view_set_justification (text, GTK_JUSTIFY_CENTER);
+          else if (g_str_equal (str, "right"))
+            gtk_text_view_set_justification (text, GTK_JUSTIFY_RIGHT);
+          else
+            /* ignore this attempted change */
+            return;
+
+          g_simple_action_set_state (action, state);
+        """
+            
     def activate_toggle(window, action, data=None):
         action.change_state(GLib.Variant('b', not action.get_state()))
     
@@ -82,8 +176,15 @@ class Window(Gtk.ApplicationWindow):
             self.unfullscreen()
         action.set_state(state)
         
-    def on_cut_activate(self, action, data=None):
-        print("cut")
+    def copy(self, action, state, textview):
+        # textview added as user data
+        clipboard = textview.get_clipboard(Gdk.SELECTION_CLIPBOARD)
+        textview.get_buffer().copy_clipboard(clipboard)
+    
+    def paste(self, action, state, textview):
+        # textview added as user data
+        clipboard = textview.get_clipboard(Gdk.SELECTION_CLIPBOARD)
+        textview.get_buffer().paste_clipboard(clipboard)
                                        
 class BloatPad(Gtk.Application):
     def __init__(self):
@@ -96,7 +197,7 @@ class BloatPad(Gtk.Application):
         self.connect("activate", self.on_activate)
 
     def about_activated(self, action, data=None):
-        Gtk.AboutDialog(program_name="Python Bloatpad",
+        dialog = Gtk.AboutDialog(program_name="Python Bloatpad",
                                  title="About Bloatpad",
                                  comments="Not much to say, really.")
         dialog.run()
@@ -135,6 +236,7 @@ class BloatPad(Gtk.Application):
         self.set_menubar(builder.get_object("menubar"))
         self.set_app_menu(builder.get_object("app-menu"))
 
+
 def install_excepthook():
     """ Make sure we exit when an unhandled exception occurs. """
     old_hook = sys.excepthook
@@ -148,5 +250,6 @@ def install_excepthook():
 if __name__ == "__main__":
     install_excepthook()
     app = BloatPad()
+    app.add_accelerator("F11", "win.fullscreen", None); # <-- does not work
     app.run(sys.argv)
     
